36b7c0c61d9c86f30b1b1ccc58142d5833ae6dd5
k2lawbaaba <kolawole.abiodun2016@gmail.com>
2023-07-21 17:13:13 +0100

    update on search

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,35 @@
 import React from "react";
 import "../styles.css";
-import KeyboardArrowRightIcon   from '@mui/icons-material/KeyboardArrowRight';
+import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
 import Details from "./Details";
 
-const header=(prop)=>{
-    return<div className="header">
-        <h3>IP address Tracker</h3>
-            <form action="">
-            <div className="searchBar">
-            <input placeholder="Search for an IP or domain..." type="text" />
-            <button type="submit"><KeyboardArrowRightIcon sx={{paddingTop:"10px", borderRadius:"50px", width:"20px" }} fontSize="medium"/></button>
-            </div>
-            </form>
-            <p className="warning"></p>
-            <div className="infoTray">
-                <Details title="IP ADDRESS" info={prop.ip_address} /><hr/>
-                <Details title="LOCATION" info={prop.location} /><hr/>
-                <Details title="TIMEZONE" info={prop.timezone} /><hr/>
-                <Details title="ISP" info={prop.isp} />
-
-            </div>
-
+const header = (prop) => {
+  return (
+    <div className="header">
+      <h3>IP address Tracker</h3>
+      <form action="">
+        <div className="searchBar">
+          <input placeholder="Search for an IP or domain..." type="text" />
+          <button type="submit">
+            <KeyboardArrowRightIcon
+              sx={{ paddingTop: "10px", borderRadius: "50px", width: "22px" }}
+              fontSize="medium"
+            />
+          </button>
+        </div>
+      </form>
+      <p className="warning"></p>
+      <div className="infoTray">
+        <Details title="IP ADDRESS" info={prop.ip_address} />
+        <hr />
+        <Details title="LOCATION" info={prop.location} />
+        <hr />
+        <Details title="TIMEZONE" info={prop.timezone} />
+        <hr />
+        <Details title="ISP" info={prop.isp} />
+      </div>
     </div>
-}
+  );
+};
 
 export default header;
